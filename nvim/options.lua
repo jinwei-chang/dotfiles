ecf084f09e96b5caae44aded277be346be6555e9
Jin-Wei Chang <jinwei.chang1221@gmail.com>
2025-11-03 12:26:47 +0800

    update: nvim ocnfigs

---
+++ nvim/options.lua
@@ -0,0 +1,23 @@
+
+
+vim.o.number = true
+vim.o.cursorline = true
+vim.o.hlsearch = true
+vim.o.laststatus = 2
+vim.o.wrap = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.smarttab = true
+
+vim.o.encoding = "utf-8"
+vim.o.mouse = "a"
+vim.o.termguicolors = true
+
+vim.cmd("filetype plugin indent on")
+vim.cmd("syntax enable")
+
+
+
