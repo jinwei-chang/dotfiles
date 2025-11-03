843c5e0446b05c8c7cdb263d1b12a344e2776b6e
Jin-Wei Chang <jinwei.chang1221@gmail.com>
2025-11-03 14:22:34 +0800

    update: install script and tmux bash version

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # nvim
-ln -r -s ./nvim ~/.config/nvim 
+ln -rsn ./nvim $HOME/.config/nvim
 
 # vim
 # ln -r -s ./vim/vim-init.vim ~/.vimrc
 
 # tmux 
-ln -r -s ./tmux/tmux.conf ~/.tmux.conf
+ln -r -s ./tmux/tmux.conf $HOME/.tmux.conf
